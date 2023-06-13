8590a993fdd10caaeacd4c1b6004539f70e8eaf4
bwackwat <john.paul.hayes2.lucid@gmail.com>
2023-06-13 12:00:04 -0600

    Upgrades.

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import time
+import sys
+
 print("Hello, world!")
+
+count = 0
+while count < 60:
+    time.sleep(1)
+    print(f"Hello {count}", flush=True)
+    # print("Hello " + count)
+    count += 1
